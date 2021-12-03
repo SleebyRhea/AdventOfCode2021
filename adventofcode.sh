089a556e46c934174275f099388b2cf9ca5fafc0
Alison Wyatt <sleepyfugu@gmail.com>
2021-12-02 21:37:17 -0500

    Modify adventofcode.sh

--- adventofcode.sh
+++ adventofcode.sh
@@ -5,21 +5,8 @@
 #   problems in Shell
 #
 
-Main()
-{
-  if ! test -d days || test -d input
-  then
-    echo "Run this from a location with a 'days' directory, and an 'input' directory" >&2
-    exit 1
-  fi
-}
-
-files=''
-
-for cmd in find sed
-do
-  command -v "$cmd" >&/dev/null \
-    && . "$cmd"
-done
-
-Main
+if ! test -f "$FILE"
+then
+  echo "Can't find ${FILE}, please place it in input/"
+  exit 1
+fi
