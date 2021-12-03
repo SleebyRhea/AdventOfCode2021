5f0e0fc0def4fd39a8c3aad82783d7d9cd8fb307
Alison Wyatt <sleepyfugu@gmail.com>
2021-12-02 23:47:35 -0500

    Add vector

--- adventofcode.h
+++ adventofcode.h
@@ -4,8 +4,33 @@
 #include <unistd.h>
 
 #define __STRING_START_SIZE 1000
+#define __ZERO              "0"
 //#define __DEBUG  1
 
+
+typedef struct vector
+{
+  uint64_t x;
+  uint64_t y;
+  uint64_t z;
+} vec3;
+
+/**
+ * @brief Create a new instance of Vec3 and malloc it to 0
+ * 
+ * @param x 
+ * @param y 
+ * @param z 
+ * @return vec3* 
+ */
+vec3 * vec3_new(uint64_t x, uint64_t y, uint64_t z)
+{
+  vec3 *v = calloc(1, sizeof(vec3));
+  v->x = 0;
+  v->y = 0;
+  v->z = 0;
+  return v;
+}
 
 /**
  * @brief Debugging print function
@@ -37,4 +62,11 @@ long howBigIsThis(FILE *f)
   free(pos);
 
   return size;
+}
+
+int isNumber(char *query)
+{
+  if (atoll(query) || !strcmp(query, __ZERO))
+    { return 1; }
+  return 0;
 }
